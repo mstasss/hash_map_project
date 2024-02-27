0f12f29e49a40c255c508128d2fc649d1a6eada1
Vickhammer <tvickhammer@gmail.com>
2024-02-26 20:00:57 -0700

    IntSet should be working. Cannot run rspec tests on Pc, :( but tested on the file itself

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -38,47 +38,52 @@ end
 class IntSet
 
   attr_reader :store
+
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets+1) { Array.new }
+    @store = Array.new(num_buckets) { Array.new }
   end
 
+
   def insert(num)
+    return false if @store.include?(num)
+    @store[num] << num
+    @store
+    # #   @store[num][0] = true unless @store[num][0]
+    # # end
 
-    #   @store[num][0] = true unless @store[num][0]
-    # end
+    # raise "error" if num > @store.length
 
-    raise "error" if num > @store.length
-
-    # if !@store.include?(num) && num <= @store.length
-       @store[num] << num #unless @store[num][0]
-       p @store
-    # end
-    true
+    # # if !@store.include?(num) && num <= @store.length
+    #    @store[num] << num #unless @store[num][0]
+    #    p @store
+    # # end
+    # true
   end
 
   def remove(num)
-    if @store[num][0] == true
-      @store[num][0] = false
-    else
-      return nil
-    end
+
+    @store[num].delete(num)
+    # if @store[num][0] == true
+    #   @store[num][0] = false
+    # else
+    #   return nil
   end
 
   def include?(num)
-    return true if @store[num][0] == true
-
+    return true if @store[num].include?(num)
+    false
     # @store.each do |subarray| #[[],[],[this one]]
     #   subarray[num]=false
     # end
 
     # @store[num][0] == true
-
   end
 
 
   private
 
   def [](num)
+    return @store[num % 20]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -90,6 +95,10 @@ end
 test = IntSet.new
 # p test.store
 p test.insert(5)
+p test.insert(19)
+p test.include?(5)
+test.remove(5)
+p test
 
 class ResizingIntSet
   attr_reader :count
