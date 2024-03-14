309565b0364cd5ac5afa153e58577c960261d139
Marie Stassinopoulos <marie.stassi@gmail.com>
2024-03-14 15:04:12 -0700

    got part two working

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -116,7 +116,7 @@ class ResizingIntSet
     num_buckets.times {@store << Array.new}
     p num_buckets
     (0..(num_buckets/2)).each do |i|
-      if !self[i].empty? 
+      if !self[i].empty?
         self[i].each do |el|
           self[i].delete(el)
           self[el] << el
--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,14 @@ end
 
 class Array
   def hash
+    return 0000000 if self == []
+    self.map(&:to_s).join().to_i
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).join().to_i
   end
 end
 
@@ -16,6 +19,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.each_value.flatten.chars.map(&:ord).join().to_i
+    # self.flatten.map(&:to_s).join().to_i
+
   end
 end
+
+# test = Array.newa.fla
