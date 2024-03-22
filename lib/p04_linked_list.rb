7f5d677ce13440aba0fcc2bc9508185c7588227b
Vickhammer <tvickhammer@gmail.com>
2024-03-21 17:17:42 -0700

    finished linked list, slow at work. Re-read the instructions and we are not supposed to modify head or tail. They are empty nodes so it changed a few things. Alot of the tests ran on the first and last methods which we had wrong. lots of banging my head on this one.

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,4 +1,5 @@
 class Node
+  include Enumerable
   attr_reader :key
   attr_accessor :val, :next, :prev
 
@@ -14,13 +15,25 @@ class Node
   end
 
   def remove
+    self.prev.next = self.next
+    self.next.prev = self.prev
+    self.prev = nil
+    self.next = nil
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @tail.prev = @head
+    @head.next = @tail
   end
 
   def [](i)
@@ -29,34 +42,77 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    self.each do |node| 
+      if node.key == key 
+        return true
+      end
+    end
+    false
   end
 
   def append(key, val)
+
+
+    new_node = Node.new(key, val)
+
+    @tail.prev.next = new_node
+    
+    new_node.prev = @tail.prev
+    new_node.next = @tail
+    
+    @tail.prev = new_node
+
+
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
+    
   end
 
   def remove(key)
+    self.each do |node| 
+      if node.key == key
+        node.remove
+        return nil
+      end
+    end
   end
 
   def each
+    current_node = @head.next
+    until current_node == @tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
+
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
